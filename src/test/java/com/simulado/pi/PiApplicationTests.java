024b2b89bb642db7b22313cfd08a084a784f3349
Miqueias Ayron <miqueiasamf@al.insper.edu.br>
2026-09-15 16:44:20 -0300

    Initial Commit

---
+++ src/test/java/com/simulado/pi/PiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.simulado.pi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
